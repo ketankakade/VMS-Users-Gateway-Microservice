1cac9348123d27c7373d6e4c8696507312ea17ce
Ketan Kakade <ketan.kakade1@hp.com>
2020-08-21 11:25:43 +0530

    Added users gateway service

---
+++ src/main/java/com/quest/vms/VmsUsersGatewayMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.quest.vms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmsUsersGatewayMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmsUsersGatewayMicroserviceApplication.class, args);
+	}
+
+}
